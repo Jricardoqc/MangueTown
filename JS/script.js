8a04b1f6ffd325a11bf3cb740f3b197c2aff6ad8
João Ricardo <rjoao148@gmail.com>
2024-12-03 21:06:42 -0300

    mais mudanças bobas em eventinhos

--- JS/script.js
+++ JS/script.js
@@ -2,40 +2,48 @@ document.addEventListener("DOMContentLoaded", async () => {
   const carrossel = document.getElementById("carrossel-eventinhos");
 
   try {
- 
     const response = await fetch("http://localhost:5000/api/events");
     const events = await response.json();
- 
+
     carrossel.innerHTML = "";
- 
+
     events.forEach((event) => {
-      
       const eventElement = document.createElement("div");
-      eventElement.classList.add("eventinho");
 
       eventElement.innerHTML = `
+      <a href="" class="eventinho">
         <img src="http://localhost:5000${event.image}" alt="${event.name}">
         <div id="descricao-eventinho">
           <span class="data">${event.date}</span>
           <p>${event.name}</p>
           <span>${event.location}</span>
         </div>
+      </a>
       `;
 
       carrossel.appendChild(eventElement);
     });
 
     function moveCarrossel(direction) {
-      const scrollAmount = carrossel.clientWidth / 3;  
-      
+      const scrollAmount = carrossel.clientWidth / 3;
+      var i = 0;
+      var valorAtual = i;
 
       if (direction === "next") {
-        carrossel.scrollLeft += scrollAmount;  
-            } else if (direction === "prev") {
-        carrossel.scrollLeft -= scrollAmount; 
-            }
+        carrossel.scrollLeft += scrollAmount;
+        valorAtual = valorAtual + 1;
+      } else if (direction === "prev") {
+        carrossel.scrollLeft -= scrollAmount;
+        valorAtual =  valorAtual - 1;
+      }
+      console.log(valorAtual)
     }
- 
+
+    function changeButton(scrollAmount) {
+      if (scrollAmount > 0) {
+      }
+    }
+
     const prevButton = document.getElementById("prev-btn");
     const nextButton = document.getElementById("next-btn");
 
@@ -45,7 +53,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     console.error("Erro ao carregar os eventos:", error);
   }
 
-  function hover() {
-    
-  }
+  function hover() {}
+
 });
