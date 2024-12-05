a8a02c2f56420a8756dfadf605e07adf4bedb84c
brunno <brunnolinker@gmail.com>
2024-12-05 09:20:18 -0300

    aaaa

--- JS/script-events.js
+++ JS/script-events.js
@@ -16,3 +16,22 @@ document.addEventListener("DOMContentLoaded", () => {
     prevButton.addEventListener("click", () => moveCarrossel("prev"));
     nextButton.addEventListener("click", () => moveCarrossel("next"));
   });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const carrossel = document.querySelector(".events-carrossel");
+    const prevButton = document.querySelector("#preve-btn");
+    const nextButton = document.querySelector("#nexte-btn");
+   
+    const scrollAmount = 300;  
+   
+    function moveCarrossel(direction) {
+      if (direction === "next") {
+        carrossel.scrollLeft += scrollAmount; 
+      } else if (direction === "prev") {
+        carrossel.scrollLeft -= scrollAmount;  
+      }
+    }
+   
+    prevButton.addEventListener("click", () => moveCarrossel("prev"));
+    nextButton.addEventListener("click", () => moveCarrossel("next"));
+  });
